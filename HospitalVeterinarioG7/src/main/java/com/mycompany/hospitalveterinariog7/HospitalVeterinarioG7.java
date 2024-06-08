98d7125fd3a68cb2e17484c45cd90c85e73ee302
Johan Nicolas Vanegas Millan <johanvanegas328720@correo.itm.edu.co>
2024-06-07 21:44:31 -0500

    add: creacion proyecto Java

---
+++ HospitalVeterinarioG7/src/main/java/com/mycompany/hospitalveterinariog7/HospitalVeterinarioG7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hospitalveterinariog7;
+
+/**
+ *
+ * @author nicol
+ */
+public class HospitalVeterinarioG7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
